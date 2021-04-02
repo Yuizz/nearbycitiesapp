5579bbe340f632f18d8620fb5363e920dbf80c1b
Julian Abelardo Gonzalez Rodriguez <julianglz270@gmail.com>
2021-04-01 19:07:51 -0500

    Added isRequired to input fields, validation and isLoading to submit button, improved the copyButton with the hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import { Box, Flex, Heading, Stack } from '@chakra-ui/layout';
-import { FormControl, FormLabel } from '@chakra-ui/form-control';
-import { Input } from '@chakra-ui/input';
-import { Button } from '@chakra-ui/button';
 import {
-  NumberDecrementStepper, NumberIncrementStepper, NumberInput,
-  NumberInputField, NumberInputStepper
-} from '@chakra-ui/number-input';
+  Box, Flex, Heading,
+  FormControl, FormLabel, Input, Button,
+  NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper,
+  Tooltip, InputGroup, InputRightElement,
+  useClipboard
+} from '@chakra-ui/react'
 import { useState } from 'react'
-import { CircularProgress } from '@chakra-ui/progress';
-import { Tooltip } from '@chakra-ui/tooltip';
 
 function App() {
   const [latitude, setLatitude] = useState('')
-  const [longitude, setLongitude] = useState('null')
+  const [longitude, setLongitude] = useState('')
   const [numberOfCities, setNumberOfCities] = useState(4)
   const [cities, setCities] = useState('')
   const [status, setStatus] = useState(false)
+  const { hasCopied, onCopy } = useClipboard(cities)
 
   const getData = (event) => {
     event.preventDefault()
-    
+    event.target.style.isLoading = true
+
     const link = `https://nearby-cities.netlify.app/.netlify/functions/search?latitude=${latitude}&longitude=${longitude}`
     
     fetch(link)
@@ -29,10 +27,6 @@ function App() {
     .then(data => formatData(data))
     
     setStatus(true)
-  }
-
-  const copyCitiesToClipboard = () => {
-    navigator.clipboard.writeText(cities)
   }
   
   const formatData = (data) => {
@@ -53,13 +47,6 @@ function App() {
     return decimalCoords
   }
 
-  const Status = () => {
-    if(!status) return ''
-    return (
-      <CircularProgress isIndeterminate color='green.300' size='20px' pr={15}/>
-    )
-  }
-
   return (
     <div className="App">
       <Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
@@ -72,26 +59,30 @@ function App() {
           py={4}
           textAlign='center'
         >
-        <Box my={8}>
-          <Heading>Encontrar ciudades cercanas</Heading>
-        </Box>
+          <Box my={8}>
+              <Heading>Encontrar ciudades cercanas</Heading>
+          </Box>
           <form>
-            <FormControl>
+            <FormControl isRequired>
               <FormLabel>Latitud: </FormLabel>
               <Input
                 placeholder="Ingresa la latidud"
                 onChange={event => setLatitude(coordsToDecimal(event.currentTarget.value))}
               ></Input>
             </FormControl>
-            <FormControl>
+
+            <FormControl isRequired>
               <FormLabel>Longitud: </FormLabel>
               <Input
+                aria-required
                 placeholder='Ingresa la longitud'
                 onChange={event => setLongitude(coordsToDecimal(event.currentTarget.value))}
               ></Input>
             </FormControl>
+
             <FormControl>
-                <FormLabel>Numero de ciudades: </FormLabel>
+              <FormLabel>Numero de ciudades: </FormLabel>
+              
               <NumberInput
                 defaultValue={4}
                 min={1}
@@ -104,30 +95,37 @@ function App() {
                     <NumberIncrementStepper />
                     <NumberDecrementStepper/>
                   </NumberInputStepper>
-                </NumberInput>
+              </NumberInput>
+              
             </FormControl>
 
             <Button
+              isLoading={status}
+              loadingText = 'Buscando ciudades'
               width='full'
               mt={4}
-              onClick={getData}
+              onClick={latitude && longitude ? getData : null}
             >🔍 Buscar ciudades cercanas</Button>
           </form>
 
-          <FormControl mt={10}>
-            <Stack isInline justifyContent='space-between'>
-              <FormLabel>Ciudades cercanas: </FormLabel>
-              <Status/>
-            </Stack>
-            <Stack direction='row'>
-              <Input readOnly value={cities}></Input>
-              <Tooltip label='Copy to clipboard' hasArrow closeDelay={500}>
-                <Button
-                  onClick={copyCitiesToClipboard}
-                >📋</Button>
+            <FormLabel mt={10}>Ciudades cercanas: </FormLabel>
+
+            <InputGroup>
+              <Input readOnly value={cities} />
+              <InputRightElement>
+                <Tooltip
+                  label='Copiar al portapapeles'
+                  hasArrow
+                  defaultIsOpen
+                  closeDelay={500}>
+                <Button onClick={onCopy}>
+                  {hasCopied ? '✔' : '📋'}
+                </Button>
               </Tooltip>
-            </Stack>
-          </FormControl>
+              </InputRightElement>
+            </InputGroup>
+
+
         </Box>
       </Flex>
     </div>
